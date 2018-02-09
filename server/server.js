95045591e93ebb61a59f69d06bd3e2ce5668f564
Jason Crawford <daxapps777@gmail.com>
2018-02-08 19:27:38 -0700

    Initial Commit

--- server/server.js
+++ server/server.js
@@ -7,8 +7,7 @@ const schema = require('./schema/schema');
 
 const app = express();
 
-// Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://dax:password@ds123658.mlab.com:23658/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
